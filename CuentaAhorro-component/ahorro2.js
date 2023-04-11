b0aa5fcae89bc8664dd0f50893c90f77f6059dcc
roritopra <1193102598@u.icesi.edu.co>
2023-04-11 01:31:52 -0500

    Added slider for seconf menu in inversion_ahorro

--- CuentaAhorro-component/ahorro2.js
+++ CuentaAhorro-component/ahorro2.js
@@ -74,3 +74,20 @@ function toggleDropdown(event) {
     dropdownMenu.classList.toggle("watch");
   }
 }
+
+//// Menu slider
+
+var marker = document.querySelector('#marker');
+var items = document.querySelectorAll('#page-switcher li');
+
+function indicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+
+items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    indicator(e.target);
+  });
+});
