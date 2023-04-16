484ec35157e6786d39a7a14eb0fae5e13b8a0b15
roritopra <1193102598@u.icesi.edu.co>
2023-04-15 19:11:57 -0500

    Hechas correcciones y los ultimos estilos de navegación por pagina

--- Ahorro-component/ahorro1.js
+++ Ahorro-component/ahorro1.js
@@ -81,3 +81,49 @@ function toggleDropdown(event) {
     dropdownMenu.classList.toggle("watch");
   }
 }
+
+//// Menu slider
+
+var marker = document.querySelector('#marker');
+var items = document.querySelectorAll('#page-switcher li');
+
+function indicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+
+items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    indicator(e.target);
+  });
+});
+
+//Función para cambiar de color la letra
+
+const lista = document.getElementById("page-switcher");
+const elementos = lista.getElementsByTagName("li");
+
+for (let i = 0; i < elementos.length; i++) {
+  elementos[i].addEventListener("click", function() {
+    const seleccionado = lista.getElementsByClassName("select");
+    if (seleccionado.length > 0) {
+      seleccionado[0].className = seleccionado[0].className.replace(" select", "");
+    }
+    this.className += " select";
+  });
+}
+
+//funcion para cambiar de color el svg
+
+function changeColor(event) {
+  var svgList = document.getElementsByTagName('path');
+  for (var i = 0; i < svgList.length; i++) {
+    svgList[i].setAttribute('fill', '#9F66DB');
+  }
+  var target = event.target;
+  var svg = target.querySelector('path');
+  svg.setAttribute('fill', 'white');
+}
+
+
