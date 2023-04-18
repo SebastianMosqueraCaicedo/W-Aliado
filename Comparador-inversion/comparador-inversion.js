449b78760de00d3ea7e3c281f54c4cf6e9f21ffb
Juan Camilo Munera <jcmunera1@gmail.com>
2023-04-17 20:44:01 -0500

    Linked all screens, testing phase

--- Comparador-inversion/comparador-inversion.js
+++ Comparador-inversion/comparador-inversion.js
@@ -1,107 +1,134 @@
 // Función para navegar hacia atrás
 function goBack() {
-    // Ocultar la sección de resultados y mostrar la sección de entrada de datos
-    document.querySelector(".results-section").style.display = "none";
-    document.querySelector(".input-section").style.display = "block";
+  // Ocultar la sección de resultados y mostrar la sección de entrada de datos
+  document.querySelector(".results-section").style.display = "none";
+  document.querySelector(".input-section").style.display = "block";
 }
 // Función para mostrar u ocultar el menú hamburguesa
 function toggleHamburgerMenu() {
-    const menu = document.querySelector(".hamburger-menu-container");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
+  const menu = document.querySelector(".hamburger-menu-container");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 }
 
 // Función para cerrar el menú hamburguesa al hacer clic fuera de él
 document.addEventListener("click", function (event) {
-    const menu = document.querySelector(".hamburger-menu-container");
-    const target = event.target;
-    if (
-        !target.closest(".hamburger-menu") &&
-        !target.closest(".hamburger-menu-container")
-    ) {
-        menu.style.display = "none";
-    }
+  const menu = document.querySelector(".hamburger-menu-container");
+  const target = event.target;
+  if (
+    !target.closest(".hamburger-menu") &&
+    !target.closest(".hamburger-menu-container")
+  ) {
+    menu.style.display = "none";
+  }
 });
 // Función para dropdown menu
 function toggleDropdown(event) {
-    const dropdownTitle = event.target;
-    const dropdownMenu = dropdownTitle.nextElementSibling;
+  const dropdownTitle = event.target;
+  const dropdownMenu = dropdownTitle.nextElementSibling;
 
-    if (dropdownMenu.classList.contains("submenu")) {
-        dropdownMenu.classList.toggle("show");
-    }
+  if (dropdownMenu.classList.contains("submenu")) {
+    dropdownMenu.classList.toggle("show");
+  }
 }
 
 // Lógica Comparador
 
-const savingsButton = document.getElementById('savings-button')
-const creditButton = document.getElementById('credit-button')
+const savingsButton = document.getElementById("savings-button");
+const creditButton = document.getElementById("credit-button");
 
-const coverPage = document.getElementById('cover-page')
-const comparator = document.getElementById('comparador')
+const coverPage = document.getElementById("cover-page");
+const comparator = document.getElementById("comparador");
 
-const bgImage = document.getElementById('cover-image')
+const bgImage = document.getElementById("cover-image");
 
-const filterContainer = document.getElementById('filter-container')
-const savingSection = document.getElementById('savings-section')
-const creditSection = document.getElementById('credit-section')
+const filterContainer = document.getElementById("filter-container");
+const savingSection = document.getElementById("savings-section");
+const creditSection = document.getElementById("credit-section");
 
+console.log(bgImage);
+savingsButton.addEventListener("click", () => {
+  coverPage.style.display = "none";
+  bgImage.src = "./assets/ahorro.png";
+  comparator.style.display = "block";
+  filterContainer.style.display = "flex";
+  savingSection.style.display = "block";
+});
 
-console.log(bgImage)
-savingsButton.addEventListener('click', () => {
-    coverPage.style.display = 'none'
-    bgImage.src = './assets/ahorro.png'
-    comparator.style.display = 'block'
-    filterContainer.style.display = 'flex'
-    savingSection.style.display = 'block'
-})
+creditButton.addEventListener("click", () => {
+  coverPage.style.display = "none";
+  bgImage.src = "./assets/credito.png";
+  comparator.style.display = "block";
+  filterContainer.style.display = "flex";
+  creditSection.style.display = "block";
+});
 
-creditButton.addEventListener('click', () => {
-    coverPage.style.display = 'none'
-    bgImage.src = './assets/credito.png'
-    comparator.style.display = 'block'
-    filterContainer.style.display = 'flex'
-    creditSection.style.display = 'block'
-})
+const comparedButton = document.getElementById("compared");
+console.log(comparedButton);
+const comparison = document.getElementById("comparison");
 
-const comparedButton = document.getElementById('compared')
-console.log(comparedButton)
-const comparison = document.getElementById('comparison')
+comparedButton.addEventListener("click", () => {
+  if (creditSection.style.display === "block") {
+    comparison.src = "./assets/compared-credito.png";
+  } else {
+    comparison.src = "./assets/compared-ahorro.png";
+  }
+  comparison.style.display = "block";
+  filterContainer.style.display = "none";
+  console.log("click");
+});
 
+const ahorroButton = document.getElementById("ahorro-switcher");
+const creditoButton = document.getElementById("credito-switcher");
 
-comparedButton.addEventListener('click', () => {
-    if (creditSection.style.display === 'block') {
-        comparison.src = './assets/compared-credito.png'
-    } else {
-        comparison.src = './assets/compared-ahorro.png'
-    }
-    comparison.style.display = 'block'
-    filterContainer.style.display = 'none'
-    console.log('click')
-})
+ahorroButton.addEventListener("click", () => {
+  coverPage.style.display = "none";
+  bgImage.src = "./assets/ahorro.png";
+  comparator.style.display = "block";
+  filterContainer.style.display = "flex";
+  savingSection.style.display = "block";
+  creditSection.style.display = "none";
+  comparison.style.display = "none";
+});
 
-const ahorroButton = document.getElementById('ahorro-switcher')
-const creditoButton = document.getElementById('credito-switcher')
+creditoButton.addEventListener("click", () => {
+  coverPage.style.display = "none";
+  bgImage.src = "./assets/credito.png";
+  comparator.style.display = "block";
+  filterContainer.style.display = "flex";
+  savingSection.style.display = "none";
+  creditSection.style.display = "block";
+  comparison.style.display = "none";
+});
+///////////⬇️ Código del footer
 
-ahorroButton.addEventListener('click', () => {
-    coverPage.style.display = 'none'
-    bgImage.src = './assets/ahorro.png'
-    comparator.style.display = 'block'
-    filterContainer.style.display = 'flex'
-    savingSection.style.display = 'block'
-    creditSection.style.display = 'none'
-    comparison.style.display = 'none'
-})
+var dropdownButtonFooter = document.getElementById("dropdown-button-footer");
+var dropdownFooter = document.getElementById("dropdown-footer");
 
-creditoButton.addEventListener('click', () => {
-    coverPage.style.display = 'none'
-    bgImage.src = './assets/credito.png'
-    comparator.style.display = 'block'
-    filterContainer.style.display = 'flex'
-    savingSection.style.display = 'none'
-    creditSection.style.display = 'block'
-    comparison.style.display = 'none'
-})
+dropdownButtonFooter.addEventListener("click", function () {
+  dropdownFooter.classList.toggle("vision");
+});
+
+var dropdownButtonFooter2 = document.getElementById("dropdown-button-footer2");
+var dropdownFooter2 = document.getElementById("dropdown-footer2");
+
+dropdownButtonFooter2.addEventListener("click", function () {
+  dropdownFooter2.classList.toggle("vision");
+});
+
+var dropdownButtonFooter3 = document.getElementById("dropdown-button-footer3");
+var dropdownFooter3 = document.getElementById("dropdown-footer3");
+
+dropdownButtonFooter3.addEventListener("click", function () {
+  dropdownFooter3.classList.toggle("vision");
+});
+
+var dropdownButtonFooter4 = document.getElementById("dropdown-button-footer4");
+var dropdownFooter4 = document.getElementById("dropdown-footer4");
+
+dropdownButtonFooter4.addEventListener("click", function () {
+  dropdownFooter4.classList.toggle("vision");
+});
