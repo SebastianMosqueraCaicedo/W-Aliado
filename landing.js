9ce8276f1bcea669fe2d036a08af5043de764d15
Juan Camilo Munera <jcmunera1@gmail.com>
2023-05-01 18:29:59 -0500

    Created navbar component, added it to landing page.

--- landing.js
+++ landing.js
@@ -14,27 +14,6 @@ function toggleHamburgerMenu() {
   }
 }
 
-// Función para cerrar el menú hamburguesa al hacer clic fuera de él
-document.addEventListener("click", function (event) {
-  const menu = document.querySelector(".hamburger-menu-container");
-  const target = event.target;
-  if (
-    !target.closest(".hamburger-menu") &&
-    !target.closest(".hamburger-menu-container")
-  ) {
-    menu.style.display = "none";
-  }
-});
-// Función para dropdown menu
-function toggleDropdown(event) {
-  const dropdownTitle = event.target;
-  const dropdownMenu = dropdownTitle.nextElementSibling;
-
-  if (dropdownMenu.classList.contains("submenu")) {
-    dropdownMenu.classList.toggle("show");
-  }
-}
-
 // Card slider funciones
 
 const slider = document.querySelector(".slider");
