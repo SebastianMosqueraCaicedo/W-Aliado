facdcedce6a565c044eb7d3fcbc790390205d72b
Juan Camilo Munera <jcmunera1@gmail.com>
2023-05-21 12:41:03 -0500

    4 sections done, Caracteristicas still have display all bug.

--- Credito/credito.js
+++ Credito/credito.js
@@ -75,3 +75,18 @@ function changeColor(event) {
   var svg = target.querySelector("path");
   svg.setAttribute("fill", "white");
 }
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
