901ad794d119afb0b2d6b399cc6d905c36671b1f
roritopra <1193102598@u.icesi.edu.co>
2023-05-23 12:23:30 -0500

    Added more screens blog, ended mobile

---
+++ Foro-w/foro.js
@@ -0,0 +1,19 @@
+var dropdownButton = document.getElementById('dropdown-button');
+var dropdownMenu = document.getElementById('dropdown-menu');
+
+dropdownButton.addEventListener('click', function() {
+  dropdownMenu.classList.toggle('show');
+});
+
+
+function toggleFilter() {
+  var div = document.getElementById("menu");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+    document.body.style.overflow = "hidden";
+  } else {
+    div.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
