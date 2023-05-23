7a915667b5fd89f6880f4144e6472498cb9549fa
tabo <1005893565@u.icesi.edu.com>
2023-05-23 13:52:48 -0500

    finish up mobile and desk button

--- Boton-Test/boton-ayuda-component.js
+++ Boton-Test/boton-ayuda-component.js
@@ -6,14 +6,18 @@ const botonChatFull = document.createElement('div');
 // funciones circumstanciales
 // opaca el fondo y abre los botones. Cierra el chat si esta abierto
 function toggleExtraButtons(){
+	let wW = window.innerWidth;
+	console.log(wW);
 	for (let j = 0; j < buttsExtra.length; j++){
 		if(buttsExtra[j].style.display === "flex"){
 			buttsExtra[j].style.display = "none";
+			mobilePipe.style.display = "none";
 			botonOpac.style.display = "none";
 			botonChat.style.display = "none";
 		} else {
 			buttsExtra[j].style.display = "flex";
 			botonOpac.style.display = "block";
+			mobilePipe.style.display = "block";
 		}
 	}
 }
@@ -110,8 +114,105 @@ botonFull.innerHTML = `
 	height: 100%;
 	background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.3));
 }
+
+.mobile-pipe {
+	display: none;
+}
+
+@media (max-width: 1200px){
+
+	.boton-div {
+		position: fixed;
+		bottom: 50px;
+		right: 20px;
+	}
+
+	/* Modo estandar */
+
+	.mobile-pipe {
+		position: absolute;
+		width: 50px;
+		height: 170px;
+		background-color: white;
+		border-radius: 20%;
+		top: -180px;
+		right: 5px;
+	}
+		
+	.boton-asesoria {
+		right: 15px;
+		top: -170px;
+	}
+
+	.boton-chat {
+		cursor: pointer;
+		right: 15px;
+		top: -90px;
+	}
+
+	.boton-pqrs {
+		right: 15px;
+		top: -50px;
+	}
+
+	.boton-tutoriales {
+		right: 15px;
+		top: -130px;
+	}
+
+	.boton-ayuda {
+		cursor: pointer;
+		width: 60px;
+		height: 60px;
+		padding: 10px;
+		background-color: #FF8833 ;
+		border-radius: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.botones-all {
+		width: 30px;
+		height: 30px;
+		background-color: #FFFFFF;
+		border-radius: 100%;
+		box-shadow: 0px 0px 9px 10px rgba(25,100,50,0.04);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		display: none;
+	}
+
+	/* chat */
+
+	.boton-chat-ex {
+		display: none;
+		position: fixed;
+		bottom: -5px;
+		right: 200px;
+	}
+
+	/* opacar */
+
+	#boton-opacar {
+		display: none;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		width: 100%;
+		height: 100%;
+		background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.3));
+	}
+
+
+}
+
 </style>
 	  <img src="../icons/boton-ayuda.svg" alt="" class="boton-ayuda" id="boton-ayuda">
+
+	  	<div class="mobile-pipe" id="mobile-pipe"></div>
 
 		  <div class="boton-asesoria botones-all" id="boton-asesoria">
 			  <a href="/Boton-Asesor/asesor.html">
@@ -151,6 +252,8 @@ const buttsExtra = [
 	document.getElementById('boton-tutoriales')
 ];
 
+const mobilePipe = document.getElementById('mobile-pipe');
+
 const botonAyuda = document.getElementById('boton-ayuda');
 const botonChat = document.getElementById('boton-chat-ex');
 const botonOpac = document.getElementById('boton-opacar');
@@ -159,5 +262,3 @@ const botonOpac = document.getElementById('boton-opacar');
 botonAyuda.addEventListener('click', toggleExtraButtons);
 
 buttsExtra[1].addEventListener('click', toggleBotonChat);
-
-// funciones globales constantes
