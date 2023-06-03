09de98b67d0bd78b7d998474d0ea49b3423d1852
tabo <1005893565@u.icesi.edu.com>
2023-06-03 09:44:28 -0500

    termina menu

--- Boton-Asesor/asesor.js
+++ Boton-Asesor/asesor.js
@@ -8,6 +8,13 @@ footer.style.display = "none";
 const cancelaBoton = document.getElementById('cancel');
 const cuelgaBoton = document.getElementById('cuelga');
 
+// menu
+const botonChat = document.getElementById('boton-chat-ex');
+const botonChatAsesor = document.getElementById('chatea');
+const botonHorarios = document.getElementById('horarios');
+const botonTutorial = document.getElementById('tutorial');
+const botonRegresa = document.getElementById('regresa');
+
 let isMenu = false;
 let time = 0;
 
@@ -30,7 +37,23 @@ function cancela() {
 	footer.style.display = "block";
 }
 
+function openChat(){
+	botonChat.style.display = "block";
+}
+
+function openHorarios(){
+}
+
+function openTutorial(){
+}
+
 setInterval(llama, 1500);
 
 cancelaBoton.addEventListener('click', cancela);
 cuelgaBoton.addEventListener('click', cuelga);
+
+// menu
+botonChatAsesor.addEventListener('click', openChat);
+botonHorarios.addEventListener('click', openHorarios);
+botonTutorial.addEventListener('click', openTutorial);
+botonRegresa.addEventListener('click', cuelga);
