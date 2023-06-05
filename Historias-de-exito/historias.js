871a6b86618c268abaccd4aa93080961a37ec19e
roritopra <1193102598@u.icesi.edu.co>
2023-06-04 20:02:28 -0500

    update cursos and clasificados

--- Historias-de-exito/historias.js
+++ Historias-de-exito/historias.js
@@ -46,3 +46,16 @@ items.forEach((item) => {
     indicator(e.target);
   });
 });
+
+function toggleFilter() {
+  var div = document.getElementById("clasifieds");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+    document.body.style.overflow = "hidden";
+  } else {
+    div.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
+
